b51b7aaee0380e5b391ca5ff9e28ac26287507b0
Nicolás Arias <nico.x.arias.-nd@disney.com>
2018-09-16 20:58:08 -0500

    HTML templates added for code generation

--- src/main/bin/scaffold.sh
+++ src/main/bin/scaffold.sh
@@ -3,4 +3,5 @@
 spring scaffold -n "inv_entrada:Entrada" -p \
 "inv_entrada_nivel:nivel:String\
  inv_entrada_largo:largo:Double\
- inv_entrada_ancho:ancho:Double"
+ inv_entrada_ancho:ancho:Double"\
+ -t src/main/resources/scaffold-templates
